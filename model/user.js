d3b55ee4f964575cae15f3a6d4b2a93e2dc5e1cd
KAMRUZZAMAN_LEON <leion.kamruzzaman@brilliant.com.bd>
2023-07-09 00:14:36 +0600

    added blog crud

--- model/user.js
+++ model/user.js
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema({
   last_name: { type: String, default: null },
   email: { type: String, unique: true },
   password: { type: String },
+  token: { type: String },
 });
 
 module.exports = mongoose.model("user", userSchema);
